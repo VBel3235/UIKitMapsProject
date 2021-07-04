8a9fdfa60b2bc9359e6dc7130a7057248ae3366f
VBel3235 <Vlad95.0753@gmain.com>
2021-07-04 15:21:10 +1000

    Initial Commit

---
+++ M10HomeWorkMaps/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  M10HomeWorkMaps
+//
+//  Created by Владислав Белов on 04.07.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
